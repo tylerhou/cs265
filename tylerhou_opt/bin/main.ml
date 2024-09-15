ef2bce520699014d11406f887ee29131f6706e81
Tyler Hou <t@tylerhou.com>
2024-09-15 20:43:59 +0000

    Bril parsing works

---
+++ tylerhou_opt/bin/main.ml
@@ -0,0 +1,11 @@
+open! Opt
+
+let () =
+  In_channel.input_all In_channel.stdin
+  |> Yojson.Basic.from_string
+  |> Bril.from_json
+  |> Valnum.run
+  |> Bril.to_json
+  |> Yojson.Basic.to_string
+  |> Out_channel.output_string Out_channel.stdout
+;;
---
+++ tylerhou_opt/lib/valnum.ml
@@ -0,0 +1,3 @@
+open! Core
+
+let run x = x
